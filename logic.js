a0f5a7a8888ac5451f3f9558c142d6ef2f033931
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-17 20:44:18 -0700

    Fixed the height of the buttons and textarea, made an array that represents the events stored in the planner. It doesn't save it yet but that's next

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 var currentDate = moment().format('dddd MMMM Do, YYYY');
 var currentHour = parseInt(moment().format('H'));
+var savedEvent = ["", "", "", "", "", "", "", "", "" ];
+
 $('#currentDay').text(currentDate);
 
 for (var i = 9; i < 18; i++){
@@ -11,3 +13,10 @@ for (var i = 9; i < 18; i++){
         $('#hour-'+ i).css("background-color", "#d3d3d3");
     }
 }
+
+$('.saveBtn').on('click', function(){
+    var targetedText = $(this).siblings()[1].value;
+    var whereToGo = parseInt($(this).parent().attr('id')[5] + $(this).parent().attr('id')[6]);
+    savedEvent[whereToGo - 9] = targetedText;
+    console.log(savedEvent);
+})
