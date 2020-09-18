98fc6d65883f23a986b19e4c0e94f80ebf466065
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-18 14:49:18 -0700

    Finished the program. Just needed to fix a few small bugs as it turns out. Everything saves well, and I even added a clear button to clear the calendar, both on the page and in local storage. Also updated README.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # word-day-planner
+Description:
+
+This is a simple day planner I have made that can:
+*Detect the date and time and use that data to change the date displayed and the background color of each hour on the planner. 
+*Save input text in each text area and when refreshed, show the inputted data in the same text area. 
+
+The HTML and CSS for the page were pre-made, but I added and styled a button to clear the calendar. 
+
+The JavaScript was written entirely by me, and uses local storage and an array to store user-submitted items so that it persists through refreshes. The clear button asks the user if they're sure they want to clear the calendar, and if confirmed resets the array to its original state and re-renders the page so the calendar is empty.
+
+Links:
+Deployed site: https://jjbidwell.github.io/work-day-planner/
+Github repository: https://github.com/jjbidwell/work-day-planner
+
+screenshots: 
+ Empty calendar:
+![Empty calendar](./assets/images/empty.png)
+
+Filled-out calendar: 
+![Filled-out calendar](./assets/images/filled.png)
+
+Data saved in local storage: 
+![Data saved in local storage](./assets/images/local-storage.png)
+
+Clear confirmation: 
+![Clear confirmation](./assets/images/clear-confirm.png)
--- logic.js
+++ logic.js
@@ -1,9 +1,13 @@
 var currentDate = moment().format('dddd MMMM Do, YYYY');
 var currentHour = parseInt(moment().format('H'));
+
+//Array that will go to local storage
 var savedEvents = ["", "", "", "", "", "", "", "", "" ];
 
 $('#currentDay').text(currentDate);
+render();
 
+//Checks current time and changes background colors
 for (var i = 9; i < 18; i++){
     if (i > currentHour){
         $('#hour-'+ i).css("background-color", "#77dd77");
@@ -14,16 +18,22 @@ for (var i = 9; i < 18; i++){
     }
 }
 
+//Accesses local storage and if it is not empty, it re-defines the savedEvents array
 function render(){
     if (localStorage.getItem('Saved events') === null){
+
     } else {
-        var iDunno = localStorage.getItem('Saved events');
-        console.log(iDunno);
+        savedEvents = localStorage.getItem('Saved events').split(',');
+    }
+
+//Aoops through each textarea and assigns its value as the corresponding array item
+    for (var i = 0; i < savedEvents.length; i++){
+       var textContent = $('#hour-' + (9+i)).find("textarea");
+        textContent.val(savedEvents[i]);
     }
 }
 
-
-
+//Assigns a value from the targeted textarea to its corresponding spot in the array, then saves the array to localstorage
 $('.saveBtn').on('click', function(){
     var targetedText = $(this).siblings()[1].value;
     var whereToGo = parseInt($(this).parent().attr('id')[5] + $(this).parent().attr('id')[6]);
@@ -31,3 +41,13 @@ $('.saveBtn').on('click', function(){
     localStorage.setItem('Saved events', savedEvents);
     render();
 })
+
+$('#clear-button').on('click', function(){
+    var clear = confirm('Are you sure you want to clear your calendar?');
+    if (clear === true){
+        console.log('Button clicked')
+        savedEvents = ["", "", "", "", "", "", "", "", "" ];
+        localStorage.setItem("Saved events", savedEvents);
+        render();
+        }
+    })
