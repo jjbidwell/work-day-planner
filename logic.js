0b584e977d3fe47f8392fe44c74e69223c1202d2
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-17 17:57:27 -0700

    Added javascript that changes the color of each hour depending on the time. I hard-coded a few different times to make sure it works, and it does

--- logic.js
+++ logic.js
@@ -1,2 +1,13 @@
 var currentDate = moment().format('dddd MMMM Do, YYYY');
+var currentHour = parseInt(moment().format('H'));
 $('#currentDay').text(currentDate);
+
+for (var i = 9; i < 18; i++){
+    if (i > currentHour){
+        $('#hour-'+ i).css("background-color", "#77dd77");
+    } else if(i === currentHour){
+        $('#hour-'+ i).css("background-color", "#ff6961");
+    } else if(i < currentHour){
+        $('#hour-'+ i).css("background-color", "#d3d3d3");
+    }
+}
